c83fe608a6aaa18b91d64ddee865bd0e99115142
Kaique Guilherme <kaiqueguilhermepereiramiranda@gmail.com>
2024-05-02 23:57:51 -0300

    cart+modalItems

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -4,13 +4,14 @@ import cartImage from '../../Assets/Cart.svg';
 import { CartContainer, ItensCart, H1ItemCart } from './Cart.style';
 
 interface CartProps {
+  onClick: () => void; 
   itemCount: number;
 }
 
-export const Cart: React.FC<CartProps> = ({ itemCount }) => {
+export const Cart: React.FC<CartProps> = ({ onClick, itemCount }) => {
   return (
     <motion.div whileHover={{ scale: 1.1 }}>
-      <CartContainer>
+     <CartContainer onClick={onClick}>
         <ItensCart>
           <img src={cartImage} alt="Cart" />
           <H1ItemCart>{itemCount}</H1ItemCart>
